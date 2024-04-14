67852b52bc23d599554d522cefb1e917ececb5b8
Ramil <ramil.mirzoev@gmail.comm>
2024-04-15 02:04:32 +0400

    add file for homework

---
+++ HomeWork_seminar2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+// Console.WriteLine("Введите число :");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+//     if ( num1 % 7 == 0 && num1 % 23 == 0)
+//     {
+//         System.Console.WriteLine($"{num1} -> yes");
+//     }
+
+// else
+// {
+//             System.Console.WriteLine($"{num1} -> no");
+// }
+
+
+
+
